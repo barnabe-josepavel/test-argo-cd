c076a2a058680cd3fa23146466f591107da1edac
Barnabe PILLIAUDIN <barnabe.pilliaudin@cyna-it.fr>
2025-08-04 12:35:58 +0200

    chore: use env var

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+  res.send(process.env.MY_SECRET);
+});
+
+app.listen(80, () => {
+  console.log('Server is running on port 80');
+});
